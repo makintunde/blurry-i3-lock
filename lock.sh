de42c6923ea6e9b9551ac564ee4ff27cc93792cc
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2017-04-28 13:31:56 +0100

    Remove eval commands

--- lock.sh
+++ lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-eval 'import -window root /tmp/screenshot.png'
-eval 'convert /tmp/screenshot.png -blur 18,5 /tmp/screenshot.png'
-eval 'i3lock -i /tmp/screenshot.png'
+import -window root /tmp/screenshot.png
+convert /tmp/screenshot.png -blur 18,5 /tmp/screenshot.png
+i3lock -i /tmp/screenshot.png
